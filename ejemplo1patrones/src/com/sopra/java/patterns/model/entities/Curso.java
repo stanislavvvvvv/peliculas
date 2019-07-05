de6bdc23e83ab2ac3420a809a92ce914d6634854
stas.hiznicenco@gmail.com <stas.hiznicenco@gmail.com>
2019-07-05 13:37:20 +0200

    fresco

--- ejemplo1patrones/src/com/sopra/java/patterns/model/entities/Curso.java
+++
@@ -1,6 +0,0 @@
-package com.sopra.java.patterns.model.entities;
-
-public class Curso {
-	
-
-}
--- ejemplo1patrones/src/com/sopra/java/patterns/model/entities/Nivel.java
+++
@@ -1,5 +0,0 @@
-package com.sopra.java.patterns.model.entities;
-
-public enum Nivel {
-	DEBUG,INFO,ERROR,WARNING;
-}
