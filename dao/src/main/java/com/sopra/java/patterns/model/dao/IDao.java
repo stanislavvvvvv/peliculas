5c5c078ab9d74869cdb2362603640b0cacb75235
stas.hiznicenco@gmail.com <stas.hiznicenco@gmail.com>
2019-07-05 13:38:06 +0200

    fresco

--- dao/src/main/java/com/sopra/java/patterns/model/dao/IDao.java
+++
@@ -1,6 +0,0 @@
-package com.sopra.java.patterns.model.dao;
-
-//Tipo T tipo generico
-public interface IDao<T> extends IGenericDao<T> {
-	T searchByName(T t);
-}
--- dao/src/main/java/com/sopra/java/patterns/model/dao/IMultipleDao.java
+++
@@ -1,8 +0,0 @@
-package com.sopra.java.patterns.model.dao;
-
-import java.util.List;
-
-public interface IMultipleDao<T,U> extends IGenericDao<T>{
-	List<T> searchByNames(String name);
-	T searchById(U id);
-}
